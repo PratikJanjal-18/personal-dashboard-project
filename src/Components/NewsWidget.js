5c31d8641811c35d9060d2a32c63fce48a556e14
Pratik Janjal <156920204+PratikJanjal-18@users.noreply.github.com>
2024-09-06 14:08:44 +0530

    Update NewsWidget.js

--- src/Components/NewsWidget.js
+++ src/Components/NewsWidget.js
@@ -11,7 +11,7 @@ function NewsWidget() {
     setError(null);
     try {
       const response = await axios.get(
-        `https://newsapi.org/v2/top-headlines?sources=the-hindu&apiKey=031c5806997d47af8f1123fb8c17339c`
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=031c5806997d47af8f1123fb8c17339c`
       );
       setNews(response.data.articles);
     } catch (error) {
